665a029a84ef9d3c004cc14596f050387705a88e
KolesnikovTimurWeb <109101206+KolesnikovTimurWeb@users.noreply.github.com>
2024-05-19 19:17:48 +0300

    Update layout.tsx

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -4,7 +4,6 @@ import Navbar from './_components/navbar'
 const MarkitingLayout = ({ children }: { children: React.ReactNode }) => {
    return (
       <div style={{ display: "flex", flexDirection: "column" }}>
-         asdasasdasd
          <Navbar />
          {children}
       </div>
