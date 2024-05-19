7860d905cd5dbffff087c71025621d01a36f1e4c
KolesnikovTimurWeb <109101206+KolesnikovTimurWeb@users.noreply.github.com>
2024-05-19 13:20:15 +0300

    Update heading.tsx

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -6,7 +6,6 @@ import Loader from '@/components/loader'
 import { SignIn, SignInButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import document from '@/public/documents.png'
-import { Link } from "react-router-dom";
 
 const Heading = () => {
    const { isAuthenticated, isLoading } = useConvexAuth()
