3cd3e97f7157e3565744ff85ca6ce6bf1bc45603
KolesnikovTimurWeb <109101206+KolesnikovTimurWeb@users.noreply.github.com>
2024-05-19 13:20:07 +0300

    Update heading.tsx

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -10,13 +10,17 @@ import { Link } from "react-router-dom";
 
 const Heading = () => {
    const { isAuthenticated, isLoading } = useConvexAuth()
+   const router = useRouter()
+    const onRedirect = () => {
+      router.push(`/documents}`);
+   };
 
    return (
       <div className={style.heading}>
          <h1>Your Ideas, Documents & Plans. Welcome to <span>Tion</span> </h1>
          <h2>Tion is connected workspace where better, faster work happens </h2>
          {isAuthenticated && !isLoading && (
-            <Link href="https://tion-bay.vercel.app/documents">Enter Tion <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"><path d="M5 12h14"></path><path d="m12 5 7 7-7 7"></path></svg> </Link>
+            <button onClick={() => onRedirect(document)} >Enter Tion <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"><path d="M5 12h14"></path><path d="m12 5 7 7-7 7"></path></svg> </button>
 
          )}
          {!isAuthenticated && !isLoading && (
