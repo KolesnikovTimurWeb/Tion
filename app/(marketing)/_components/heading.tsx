a6a45e36235319e3b66168ff16f9d2fe09f6e50c
KolesnikovTimurWeb <109101206+KolesnikovTimurWeb@users.noreply.github.com>
2024-05-19 13:26:14 +0300

    Update heading.tsx

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -19,7 +19,7 @@ const Heading = () => {
          <h1>Your Ideas, Documents & Plans. Welcome to <span>Tion</span> </h1>
          <h2>Tion is connected workspace where better, faster work happens </h2>
          {isAuthenticated && !isLoading && (
-            <button onClick={() => onRedirect(document)} >Enter Tion <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"><path d="M5 12h14"></path><path d="m12 5 7 7-7 7"></path></svg> </button>
+            <button onClick={() => onRedirect()} >Enter Tion <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"><path d="M5 12h14"></path><path d="m12 5 7 7-7 7"></path></svg> </button>
 
          )}
          {!isAuthenticated && !isLoading && (
